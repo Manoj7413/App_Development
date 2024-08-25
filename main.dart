9427901d88c46979b2be948bf4a5ad79b853704e
Manoj7413 <kumharmanoj303@gmail.com>
2024-08-25 10:49:18 +0530

    Initial commit

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:app_1/First.dart';
+import 'package:app_1/Home.dart';
+import 'package:flutter/material.dart';
+
+import 'Loginpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily:'Rohit'),
+      title: 'Flutter Demo',
+      home:Loginpage(),
+    );
+  }
+}
